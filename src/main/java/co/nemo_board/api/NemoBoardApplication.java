58c5e1c42f9382b43c70acc326b87340fd702361
yonghwankim-dev <dragonbead95@naver.com>
2024-09-07 15:24:11 +0900

    initial commit

---
+++ src/main/java/co/nemo_board/api/NemoBoardApplication.java
@@ -0,0 +1,13 @@
+package co.nemo_board.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemoBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemoBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/nemo_board/api/NemoBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package co.nemo_board.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemoBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
